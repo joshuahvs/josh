35c427e0be2e92dad097fd240edc99dad1eff05e
vito1012buss <thebestvitodiaries@gmail.com>
2024-09-11 11:35:46 +0700

    menyelesaikan kesalahpahaman tugas 2

--- main/views.py
+++ main/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_main(request):
-    context = {
-        'npm' : '2306165540',
-        'name': 'Joshua Hans Vito Soehendra',
-        'class': 'PBP A',
-        'description' : 'A Make-up Store'
-    }
 
-    return render(request, "main.html", context)
+    products = [
+    {'name': 'lipgloss', 'price': 1000000, 'description': 'sun glaze lipgloss', 'quantity': 100},
+    {'name': 'blush on', 'price': 1000000, 'description': 'sugar plum blush on', 'quantity': 89}
+    ]
+
+    return render(request, "main.html", {'products': products})
+
+    # products = [{
+    #     'name': 'lipgloss',
+    #     'price': 10000000,
+    #     'description': 'hellooo',
+    #     'quantity': 1
+    # },
+    # {
+    #     'name': 'blush on',
+    #     'price': 10000000,
+    #     'description': 'lol',
+    #     'quantity': 1
+    # }]
+
+    # return render(request, "main.html", products)
