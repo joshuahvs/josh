abca91ec994002da560b3664adb09c9e49b7aadf
vito1012buss <thebestvitodiaries@gmail.com>
2024-09-22 20:14:45 +0700

    Membuat register page, login page, logout

--- main/views.py
+++ main/views.py
@@ -1,10 +1,18 @@
+import datetime
 from django.shortcuts import render, redirect
 from main.forms import joshShopEntryForm
 from main.models import joshItem
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+@login_required(login_url='/login')
 def show_main(request):
     josh_item_entries = joshItem.objects.all()
     products = [
@@ -13,7 +21,10 @@ def show_main(request):
     ]
     context = {
         'products': products,
-        'josh_item_entries': josh_item_entries
+        'josh_item_entries': josh_item_entries,
+        # 'last_login': request.COOKIES['last_login'],
+        'last_login': request.COOKIES.get('last_login', 'Not Available'),
+
     }
 
     return render(request, "main.html", context)
@@ -41,3 +52,34 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = joshItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
