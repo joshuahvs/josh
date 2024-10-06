7d8c173d4648f1acb9d08e017062b0b381b5f922
vito1012buss <thebestvitodiaries@gmail.com>
2024-10-07 02:05:20 +0700

    menambahkan add item ajax

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_item_entry, edit_item, delete_item
+from main.views import show_main, create_item_entry, edit_item, delete_item, add_item_entry_ajax
 from main.views import show_xml, show_json,show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user
 
@@ -16,5 +16,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('edit-item/<uuid:id>', edit_item, name='edit_item'),
-    path('delete/<uuid:id>', delete_item, name='delete_item')
+    path('delete/<uuid:id>', delete_item, name='delete_item'),
+    path('create-item-entry-ajax', add_item_entry_ajax, name='add_item_entry_ajax')
 ]
--- main/views.py
+++ main/views.py
@@ -9,11 +9,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 
 @login_required(login_url='/login')
 def show_main(request):
-    josh_item_entries = joshItem.objects.filter(user=request.user)
     products = [
     {'name': 'lipgloss', 'price': 1000000, 'description': 'sun glaze lipgloss', 'quantity': 100},
     {'name': 'blush on', 'price': 1000000, 'description': 'sugar plum blush on', 'quantity': 89},
@@ -21,7 +22,6 @@ def show_main(request):
     context = {
         'name': request.user.username,
         'products': products,
-        'josh_item_entries': josh_item_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -36,6 +36,26 @@ def create_item_entry(request):
         return redirect('main:show_main')
     context = {'form': form}
     return render(request,'create_item_entry.html', context)
+
+@csrf_exempt
+@require_POST
+def add_item_entry_ajax(request):
+    name = request.POST.get("name")
+    price = request.POST.get("price")
+    description = request.POST.get("description")
+    quantity = request.POST.get("quantity")
+    user = request.user
+
+    new_item = joshItem(
+        name=name, 
+        price=price,
+        description=description,
+        quantity=quantity,
+        user=user
+    )
+    new_item.save()
+
+    return HttpResponse(b"CREATED", status=201)
 
 def edit_item(request, id):
     # Get mood entry berdasarkan id
@@ -56,11 +76,11 @@ def delete_item(request,id):
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request):
-    data = joshItem.objects.all()
+    data = joshItem.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = joshItem.objects.all()
+    data = joshItem.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type ="application/json")
 
 def show_xml_by_id(request, id):
@@ -84,13 +104,15 @@ def register(request):
 
 def login_user(request):
    if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
-      if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
    else:
       form = AuthenticationForm(request)
    context = {'form': form}
